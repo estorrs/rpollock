979cb7b806e22cc1ec25f7541dd90e3f614b3bf8
Erik <epstorrs@gmail.com>
2020-11-10 11:05:41 -0600

    added explain function

---
+++ R/explain_predictions.R
@@ -0,0 +1,9 @@
+explain_predictions = function(explain_counts_matrix, background_counts_matrix,
+	labels, module_filepath, output_filepath, background_sample_size=100) {
+    pollock = reticulate::import('pollock')
+    explain = as.data.frame(explain_counts_matrix)
+    background = as.data.frame(background_counts_matrix)
+    explainations = pollock$wrappers$rwrappers$explain_from_dataframe(explain, background, labels,
+            module_filepath, output_filepath, background_sample_size=background_sample_size)
+    explainations
+}
